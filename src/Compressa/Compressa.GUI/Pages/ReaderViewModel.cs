9596cc90352481e7bf0fa35c2726e01a40a51570
Andras Fuchs <andras.fuchs@gmail.com>
2022-12-10 16:05:18 +0100

    Add the Reader page

---
+++ src/Compressa/Compressa.GUI/Pages/ReaderViewModel.cs
@@ -0,0 +1,39 @@
+﻿namespace Compressa.GUI.Pages;
+
+[INotifyPropertyChanged]
+public partial class ReaderViewModel
+{
+    [ObservableProperty]
+    ObservableCollection<Item> _products;
+
+    [ObservableProperty]
+    string category = ItemCategory.Business.ToString();
+
+    partial void OnCategoryChanged(string cat)
+    {
+        ItemCategory category = (ItemCategory)Enum.Parse(typeof(ItemCategory), cat);
+        _products = new ObservableCollection<Item>(
+            AppData.Items.Where(x => x.Category == category).ToList()
+        );
+        OnPropertyChanged(nameof(Products));
+    }
+
+    public ReaderViewModel()
+    {
+        _products = new ObservableCollection<Item>(
+            AppData.Items.ToList()
+        );
+    }
+
+    [RelayCommand]
+    async Task Preferences()
+    {
+        await Shell.Current.GoToAsync($"{nameof(EmptyPage)}?sub=appearance");
+    }
+
+    [RelayCommand]
+    async Task AddProduct()
+    {
+        MessagingCenter.Send<ReaderViewModel, string>(this, "action", "add");
+    }
+}
